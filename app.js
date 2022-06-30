c1de7a42ae7b56e0401361936506b0d8b7f997e8
davblight <davblight@gmail.com>
2022-06-30 12:29:49 -0600

    add backend functioning

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var app = new Vue({
         emailInput: "",
         passwordInput: "",
         nameInput: "",
+        error: "",
+        threadNameInput: "",
+        threadDescInput: "",
+        threadCatInput: "",
+        allThreads: [],
+        postBodyInput: "",
     },  
     methods: {
         showSignUp: function () {
@@ -21,6 +27,7 @@ var app = new Vue({
             this.passwordInput = "";
             this.nameInput = "";
         }, 
+        //USER LOGIN AND AUTHENTICATION LOGIC
         //GET /session - Ask the server if we are logged in
         getSession: async function () {
             let response = await fetch(`${URL}/session`, {
@@ -32,6 +39,7 @@ var app = new Vue({
                 console.log("logged in");
                 let data = await response.json();
                 console.log(data);
+                this.page = "home"
 
             } else if (response.status == 401) {
                 console.log("Not logged in")
@@ -97,9 +105,85 @@ var app = new Vue({
             } else {
                 console.log("unknown error posting /users", response.status, response)
             }
-        }
+        },
+
+        //THREAD LOGIC
+        getThread: async function () {
+            let response = await fetch(`${URL}/thread`, {
+                //method: "GET",
+                credentials: "include" //every fetch request must have this inside of it or else you basically lose your cookie
+            });
+            if (response.status == 200){
+                console.log("thread got");
+                let data = await response.json();
+                //console.log(data);
+                this.allThreads = data;
+            }
+        },
+
+        //Identify thread ID: TEST ID
+        getThreadID: async function () {
+            let response = await fetch(`${URL}/thread/62bde3739e9a3066e09b6fbb`, {
+                credentials: "include"
+            });
+            let data = await response.json();
+            console.log(data);
+        },
+
+        postThread: async function () {
+            let newThread = {name: this.threadNameInput, description: this.threadDescInput, category: this.threadCatInput};
+            let response = await fetch(`${URL}/thread`, {
+                method: "POST",
+                body: JSON.stringify(newThread),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            let body = response.json();
+            console.log(body);
+        },
+
+        //Delete thread by ID: TEST ID
+        deleteThread: async function () {
+            let response = await fetch(`${URL}/thread/62bddb5f9e9a3066e09b6f76`, {
+                method: "DELETE",
+                credentials: "include"
+            });
+            let body = response.json();
+            console.log(body);
+        },
+
+        //POST LOGIC
+
+        //Post post: TEST ID
+        postPost: async function () {
+            let newPost = {thread_id: "62bde3739e9a3066e09b6fbb", body: this.postBodyInput}
+            let response = await fetch(`${URL}/post`, {
+                method: "POST",
+                body: JSON.stringify(newPost),
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            let data = response.json();
+            console.log(data);
+        },
+        //Delete post: TEST ID
+        deletePost: async function () {
+            let response = await fetch(`${URL}/thread/62bde3739e9a3066e09b6fbb/post/62bde9889e9a3066e09b6fc8`, {
+                method: "DELETE",
+                credentials: "include"
+            });
+            let body = response.json();
+            console.log(body);
+        },
+
     },
     created: function () {
         this.getSession();
+        this.getThread();
     }
 })
